d9f2749c435025491fdd0173770ca9af4c7ce3d9
kahilkubilay <kahil_kubilay@hotmail.com>
2023-05-07 12:50:32 +0300

    static files uploaded

--- app.js
+++ app.js
@@ -1,4 +1,20 @@
-const fn = function () {
-  console.log('function working');
-};
+import express from 'express';
 
+import path from 'path';
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'template/index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Server is started from ${port} number`);
+});
